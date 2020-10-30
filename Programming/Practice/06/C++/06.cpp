1038e85372bd1d92dfdb9788cbd07653a2765319
frity <nikonovfedor36936@gmail.com>
2020-10-30 22:26:39 +0300

    cpp-sln: 06

--- Programming/Practice/06/C++/06.cpp
+++ Programming/Practice/06/C++/06.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
+#include <math.h>
 #include "./../../practice.cpp"
 
 int main () {
-	
-	
-	
+	description("Решение линейного уравнения. Введите коэффициенты: a b c");
+
+	double a, b, c;
+	std::cin >> a >> b >> c;
+
+	if (a == 0)
+	{
+		std::cout << -c / b << std::endl;
+	}
+	else
+	{
+		double descr = (b * b - 4 * a * c);
+		if (descr < 0)
+			std::cout << "Корней нет" << std::endl;
+		else if (descr == 0)
+			std::cout << "x = " << -b / (2 * a) << std::endl;
+		else
+		{
+			descr = sqrt(descr);
+			std::cout << "x1 = " << (-b + descr) / (2 * a) << std::endl;
+			std::cout << "x2 = " << (-b - descr) / (2 * a) << std::endl;
+		}
+	}
+
 	return 0;
 }
--- Programming/Practice/practice.cpp
+++ Programming/Practice/practice.cpp
@@ -6,13 +6,13 @@ void wl(int message) { std::cout << message << std::endl; }
 void wl(double message) { std::cout << message << std::endl; }
 void header()
 {
+  std::setlocale(LC_ALL, "Russian");
   wl("Никонов Фёдор, ПИ-б-о-201(2)");
   wl("----------------------------");
 }
 
 void description(std::string message)
 {
-  std::setlocale(LC_ALL, "Russian");
   header();
   wl("Условие задачи: " + message);
   wl("============================");
