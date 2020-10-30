2515531f11ada52383600d5b1a89c2ed908d9196
frity <nikonovfedor36936@gmail.com>
2020-10-30 17:01:49 +0300

    cpp-tpl, py-sln: 22

--- Programming/Practice/22/Python/22.py
+++ Programming/Practice/22/Python/22.py
@@ -1,7 +1,12 @@
-import os, re
-from typing import Pattern
+import os, re, time
 cls = lambda: os.system('clear' if os.name == 'posix' else 'cls')
 cls()
+
+class Console():
+   @staticmethod
+   def move_cursor(x, y):
+      print(f'\033[{y+1};{x+1}f', end='')
+   
 
 class FormatString(str):
    END = '\033[0m'
@@ -37,6 +42,30 @@ class FormatString(str):
    @property
    def underline(self): 
       return self._format(4)
+   @property
+   def normal(self):
+      return self
+   
+class xstr(str):
+   TAGS = {
+      'bold': 'bold',
+      'underline': 'underline',
+      'purple': 'header',
+      'blue': 'blue',
+      'cyan': 'cyan',
+      'green': 'green',
+      'yellow': 'warning',
+      'red': 'fail',
+   }
+   def __new__(cls, string, *args, **kwargs):
+      res = string
+      for tag, style in xstr.TAGS.items():
+         res = re.sub(f'<{tag}\|'+r'(.*)'+f'>', getattr(FormatString(r'\1'), style), res)
+      return super(xstr, cls).__new__(cls, res)
+   
+   def __init__(self, string: str):
+      pass
+      
    
 class Pos:
    def __init__(self, x: int, y: int):
@@ -47,15 +76,25 @@ class Pos:
    def differ(pos1, pos2):
       return Pos(pos1.x-pos2.x, pos1.y-pos2.y)
    
+class MazeChar(str):
+   def __new__(cls, pattern, *args, **kwargs):
+      # explicitly only pass pattern to the str constructor
+      return super(MazeChar, cls).__new__(cls, pattern)
+   def __init__(self, pattern, style):
+      self.style = style
+   
 class Maze:
-   WALL = r'🬀'
-   PLAYER = r'#'
-   STEP = r'⋅'
-   EXIT = r'(A|B|C|D|E|F|G)'
-   REACHED = r'(a|b|c|d|e|f|g)'
+   CHARS = {
+      'WALL': MazeChar(r'🬀',   'fail'),
+      'PLAYER': MazeChar(r'#', 'blue bold'),
+      'STEP': MazeChar(r'•',   'normal'),
+      'PREV': MazeChar(r'⋅',   'cyan'),
+      'EXIT': MazeChar(r'(A|B|C|D|E|F|G)',    'green bold'),
+      'REACHED': MazeChar(r'(a|b|c|d|e|f|g)', 'blue bold'),
+   }
    
    def __init__(self, pattern: str):
-      pattern = pattern.strip().replace('#', Maze.WALL)
+      pattern = pattern.strip().replace('#', Maze.CHARS['WALL'])
                                         
       self.pattern = pattern.split('\n')
       self.width = pattern.index('\n')
@@ -67,69 +106,66 @@ class Maze:
       self.player_y = 0
       
       
-   def _move_cursor(self, x, y):
-      print(f'\033[{y};0H')
-      formated = self._format_source(self.pattern[y][:x])
-      print(formated, end='')
-      
-      
    def _put(self, x: int, y: int, char: str):
       row = list(self.pattern[y])
       row[x] = char
       self.pattern[y] = ''.join(row)
-      self._move_cursor(x, y)
+      Console.move_cursor(x, y)
+      char = self._format_char(char)
       print(char)
       
    def _take(self, x: int, y: int):
       return self.pattern[y][x]
       
       
-   def _format_single(self, char: str, format: str, need_return=False):
+   def _format_single(self, source: str, char: str, format: str):
       format = format.split(' ')
       formated = FormatString(r'\1' if char[0] == '(' else char)
       for style in format: 
          formated = getattr(formated, style)
-      formated = re.sub(char, formated, (need_return or self.formated_pattern))
-      if (need_return):
-         return formated
-      else:
-         self.formated_pattern = formated
+      source = re.sub(char, formated, source)
+      return source
    
    def _format(self):
-      self.formated_pattern = '\n'.join(self.pattern)
+      source = '\n'.join(self.pattern)
       f = self._format_single
-      f(Maze.WALL,    'fail')
-      f(Maze.PLAYER,  'blue bold')
-      f(Maze.EXIT,    'green bold')
-      f(Maze.STEP,    'cyan')
-      f(Maze.REACHED, 'blue bold')
+      res = source
+      for char in Maze.CHARS.values():
+         res = f(res, char, char.style)
+      self.formated_pattern = res
       
-   def _format_source(self, source):
-      formated = source
+   def _format_char(self, char: str):
       f = self._format_single
+      res = char
+      for char in Maze.CHARS.values():
+         res = f(res, char, char.style)
+      return res
       
-      formated = f(Maze.WALL,    'fail'      , formated)
-      formated = f(Maze.PLAYER,  'blue bold' , formated)
-      formated = f(Maze.EXIT,    'green bold', formated)
-      formated = f(Maze.STEP,    'cyan'      , formated)
-      formated = f(Maze.REACHED, 'blue bold' , formated)
-      
-      return formated
+   
+   def move_cursor_down(self):
+      Console.move_cursor(0, self.height+2)
    
    def render(self):
-      #cls()
+      cls()
       self._format()
       undo_print = '\r' * self.height
       print(self.formated_pattern, end=undo_print)
       
    
    def player(self, x: int, y: int):
-      self._put(x, y, Maze.PLAYER)
+      pos_type = self._check_pos(Pos(x, y))
+      if (pos_type != 'free'):
+         return False
+      
+      self._put(x, y, Maze.CHARS['PLAYER'])
+      
       self.player_x = x
       self.player_y = y
       
+      return True
+      
    def step(self, x: int, y: int):
-      self._put(x, y, Maze.STEP)
+      self._put(x, y, Maze.CHARS['STEP'])
       
       
    def _check_pos(self, pos: Pos):
@@ -143,10 +179,12 @@ class Maze:
       def char_is(target):
          return re.match(target, char)
       
-      if char_is(Maze.WALL): return 'wall'
-      elif char_is(Maze.EXIT) or char_is(Maze.REACHED): return 'exit'
-      elif char_is(Maze.PLAYER): return 'player'
-      elif char_is(Maze.STEP): return 'step'
+      if char_is(Maze.CHARS['WALL']): return 'wall'
+      elif (char_is(Maze.CHARS['EXIT']) or
+           char_is(Maze.CHARS['REACHED'])): return 'exit'
+      elif char_is(Maze.CHARS['PLAYER']): return 'player'
+      elif char_is(Maze.CHARS['PREV']): return 'step'
+      elif char_is(Maze.CHARS['STEP']): return 'prev'
       else: return 'free'
       
    
@@ -165,6 +203,8 @@ class Maze:
             if new_pos_type is 'free':
                addon += [new_pos]
                self.step(new_pos.x, new_pos.y)
+            elif new_pos_type is 'prev':
+               self._put(new_pos.x, new_pos.y, Maze.CHARS['PREV'])
             elif new_pos_type is 'exit':
                exit = self._take(new_pos.x, new_pos.y)
                self._put(new_pos.x, new_pos.y, exit.lower())
@@ -183,10 +223,11 @@ class Maze:
             resume += [is_steps]
          
          if (all(not r for r in resume)):
-            return exits.keys()
+            return list(exits.keys())
+         
          
          resume = []
-               
+         time.sleep(0.08)
             
       
 
@@ -219,9 +260,20 @@ A           #   ###   #   #
 """
 maze = Maze(maze)
 maze.render()
-maze.player(4, 1)
+maze.move_cursor_down()
 
-#maze.step(3, 1)
-#maze.step(5, 1)
+print(xstr('Добро пожаловать в <blue|Лабиринт> от <purple|frity corp.>'))
+s = xstr('Введите координаты игрока <yellow|(x y)>: ')
+x, y = map(int, input(s).split(' '))
+
+player = maze.player(x-1, y-1)
+maze.render()
+if (not player):
+   maze.move_cursor_down()
+   print(xstr('<red|Ошибка:> Не верные координаты'))
+   exit()
+
 exits = maze.waterfall()
-print(exits)
+exits = ' '.join(exits)
+Console.move_cursor(0, maze.height+2)
+print(xstr(f'Найдены выходы: <purple|{exits}>'))
