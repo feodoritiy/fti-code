88b00ae6098a6e596d4ac8e895a84bd30106a4b0
frity <nikonovfedor36936@gmail.com>
2020-11-20 12:28:55 +0300

    upd readme.py and readme.md

--- Programming/readme.py
+++ Programming/readme.py
@@ -11,14 +11,22 @@ README_BASE = """## Программирование
 """
 
 import os
-table = ''
+table = []
 
-projects = os.scandir('Practice')
+projects = list(filter(lambda el: el.is_dir(), os.scandir('Practice')))
+labs = list(filter(lambda el: el.is_dir(), os.scandir('Labs')))
 for folder in projects:
    if not folder.is_dir(): continue
    folder = folder.name
-   table += f'| Практическая {folder} - [[С++]](./Practice/{folder}/C++/{folder}.cpp) [[Python]](./Practice/{folder}/Python/{folder}.py) |  |\n'
+   table += [f'| Практическая {folder} - [[С++]](./Practice/{folder}/C++/{folder}.cpp) [[Python]](./Practice/{folder}/Python/{folder}.py) | ']
+   
+for i in range(len(projects)):
+   if (i < len(labs)):
+      folder = labs[i].name
+      table[i] += f' [Лабораторная {folder}](./Labs/{folder}/README.md) |\n'
+   else:
+      table[i] += '|\n'
 
 readme = open('README.md', 'w')
-readme.write(README_BASE + table)
+readme.write(README_BASE + ''.join(table))
 readme.close()
