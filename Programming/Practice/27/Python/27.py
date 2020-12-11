8569efe5f3d75cbc5263140fc1c738f9dfe350f1
frity <nikonovfedor36936@gmail.com>
2020-12-11 23:21:15 +0300

    py-sln: 27

--- Programming/Practice/27/Python/27.py
+++ Programming/Practice/27/Python/27.py
@@ -1,1 +1,19 @@
+def pushSort(arr, element):
+	arr.append(element)
+	# insertion sort part
+	j = len(arr) - 2
+	while j >= 0 and arr[j] < element:
+		arr[j + 1] = arr[j]
+		j -= 1
+	arr[j + 1] = element
 
+
+N = input("Введите число сигналов: ")
+signal = None
+signals = list(map(int, input('Массив сигналов: ').split(' ')))
+visible = []
+
+for signal in signals:
+   pushSort(visible, signal)
+   visibleLen = len(visible)
+   print(*visible[-5:])
